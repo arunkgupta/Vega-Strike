9cf2078c3fc39681404e59512b2a86503b49f887
andy costanza <costalfy@gmail.com>
2014-03-13 14:58:33 +0100

    refactoring for include internationalization Add french po and mo file

--- data/bases/fixers/no.py
+++ data/bases/fixers/no.py
@@ -2,4 +2,4 @@ import fixers
 import Base
 
 fixers.DestroyActiveButtons ()
-Base.Message("No? Come back to me again if you change your mind.")
+Base.Message(_("No? Come back to me again if you change your mind."))
