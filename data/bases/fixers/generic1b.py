d0620e2bb7ef7fde31eb1c7bb3a30784a525aaa4
VsOBu <obuhard@yahoo.fr>
2014-03-16 17:04:23 +0100

    Update generic1b.py

--- data/bases/fixers/generic1b.py
+++ data/bases/fixers/generic1b.py
@@ -4,7 +4,7 @@ import VS
 
 fixers.DestroyActiveButtons ()
 if VS.numActiveMissions()>1:
-    Base.Message('You are already doing a mission. Finish that instead.')
+    Base.Message(_('You are already doing a mission. Finish that instead.'))
 else:
     mission_lib.BriefLastMission(1,1)
     #VS.LoadMission('internal_mission_lib.mission')
