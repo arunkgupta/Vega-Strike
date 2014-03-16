2a2e72413710b1af64d996fe15e83f3d5aa7ca56
VsOBu <obuhard@yahoo.fr>
2014-03-16 17:01:29 +0100

    Update explore_notready.py

--- data/bases/fixers/explore_notready.py
+++ data/bases/fixers/explore_notready.py
@@ -1,5 +1,5 @@
 import Base
 
-Base.Message("""Hi. According to your computer\'s data, you have not identified all six of the objects.
+Base.Message(_("""Hi. According to your computer\'s data, you have not identified all six of the objects.
 This also means that you will be unable to recieve your money until this job is completed.
-Come back to me when you are finished and you will recieve the rest of your money.""")
+Come back to me when you are finished and you will recieve the rest of your money."""))
